5a701f0c799fae890c15fde2442814616df28bf2
Jose Cabarcas <jcabarcas@picap.co>
2020-04-21 14:42:00 -0500

    [Fix] Responsive screnn home

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,9 +1,44 @@
+import { useState } from 'react'
+
 const Navbar = () => {
   const list = ['Home', 'About', 'Tech', 'Portfolio']
+
+  const [isOpen, setIsOpen] = useState(true)
+
+  const handleMenuResponsive = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <header className="header">
-      <a href="/" className="brand">José C</a>
-      <nav>
+      <div className="brand">
+        <div className="brand-logo">
+          <a href="/">José C</a>
+        </div>
+        <div className="menu">
+          <button
+            onClick={handleMenuResponsive}
+            type="button"
+            className="block text-gray-500 focus:outline-none"
+          >
+            <svg className="" viewBox="0 0 24 24">
+              {isOpen && (
+                <path
+                  fillRule="evenodd"
+                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                />
+              )}
+              {!isOpen && (
+                <path
+                  fillRule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+      </div>
+      <nav className={`${isOpen ? "block" : "hidden"}`}>
         <ul>
           {
             list.map(item => (
